1aa813275ba09b0b1006b58dc9b6f993faf5c52e
sontruongces <son.truong@codeenginestudio.com>
2022-08-30 15:46:54 +0700

    feat: init react app
    CU-3cy5cnn

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = document.getElementById("root");
+const rootEle = ReactDOM.createRoot(root);
+rootEle.render(<App />);
